0b71dceef0990d8b21616ad2e5ad11b288282ed4
xim3namartin <ximenamartin33@gmail.com>
2024-12-11 20:48:24 +0000

    modifico archivo js

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ navToggle.addEventListener("click", () => {
     }
 });
 
+
+
+
+//Funcion para posicionarse en las CARDS//
+document.getElementById('btn-mas').addEventListener('click', function () {
+    const headerHeight = document.querySelector('.header').offsetHeight; // Obtener la altura del encabezado
+    const SeccionCards = document.getElementById('seccioncard').offsetTop - headerHeight; // Ajustar la posición
+
+    // Desplazarse a la posición de la seccion de las tarjetas //
+    window.scrollTo({
+        top: SeccionCards,
+        behavior: "smooth" // Desplazamiento suave
+    });
+});
+
