300b5cd1dbd070af5cc47e47f07d1154be9947f1
xim3namartin <ximenamartin33@gmail.com>
2024-12-18 10:54:25 +0000

    agrego swiper al carrusel de la bliblioteca

--- index.js
+++ index.js
@@ -38,3 +38,21 @@ document.getElementById('btn-mas').addEventListener('click', function () {
     });
 
 
+
+//Funcion SWIPER - BIBLIOTECA//
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
