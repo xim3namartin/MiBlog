8ee802c1ca761b5dbbde5c532503c88ce8b32114
xim3namartin <ximenamartin33@gmail.com>
2025-03-31 21:56:17 +0100

    agrego sobremi.js y modifico css

---
+++ sobremi.js
@@ -0,0 +1,8 @@
+
+// funcion para mostrar parrafo//
+
+document.getElementById("btn-LeerMas").addEventListener("click", function() {
+    const parrafo = document.getElementById("parrafo");
+    parrafo.classList.toggle("oculto"); // Alterna entre mostrar y ocultar el párrafo
+  });
+  
